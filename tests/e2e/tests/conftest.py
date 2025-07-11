725d1107aa89cbfa85b23683c933a1b3bf7c1c65
jannschu <jannik.schuerg@posteo.de>
2025-07-11 15:11:45 +0200

    Use redis provider for CI tests

--- tests/e2e/tests/conftest.py
+++ tests/e2e/tests/conftest.py
@@ -1,30 +1,43 @@
 import time
 import random
+import shutil
 from pathlib import Path
 from typing import Any
 
 import pytest
 import requests
-import yaml
+import redis
 
 TRAEFIK_URL = "http://localhost"
+CONTENT_DIR_REPO = Path(".github/traefik/maintenance")
+ECHO_ROUTER = "http/routers/echo"
+REDIS_ROOT_KEY = "traefik"
 
-DYNAMIC_FILE_REPO = Path(".github/traefik/dynamic/dynamic.yml")
-
-DYNAMIC = Path(__file__).parent
-while DYNAMIC.parent and not (DYNAMIC / DYNAMIC_FILE_REPO).exists():
-    DYNAMIC = DYNAMIC.parent
-if not DYNAMIC.parent:
+CONTENT_DIRECTORY = Path(__file__).parent
+while CONTENT_DIRECTORY.parent != CONTENT_DIRECTORY and not (CONTENT_DIRECTORY / CONTENT_DIR_REPO).exists():
+    CONTENT_DIRECTORY = CONTENT_DIRECTORY.parent
+if CONTENT_DIRECTORY.parent == CONTENT_DIRECTORY:
     raise RuntimeError("Could not find dynamic.yml in the repository.")
-DYNAMIC = DYNAMIC / DYNAMIC_FILE_REPO
+CONTENT_DIRECTORY = CONTENT_DIRECTORY / CONTENT_DIR_REPO
 
-CONTENT_DIRECTORY = DYNAMIC.parent.parent / "maintenance"
+
+redis = redis.Redis("localhost")
+redis.flushdb()
+redis.mset(
+    {
+        f"{REDIS_ROOT_KEY}/{ECHO_ROUTER}/entrypoints": "http",
+        f"{REDIS_ROOT_KEY}/{ECHO_ROUTER}/service": "echo",
+        f"{REDIS_ROOT_KEY}/{ECHO_ROUTER}/rule": "PathPrefix(`/ci`)",
+        f"{REDIS_ROOT_KEY}/http/services/echo/loadBalancer/servers/port": "80",
+        f"{REDIS_ROOT_KEY}/http/services/echo/loadBalancer/servers/url": "http://echo/",
+    }
+)
 
 
 def pytest_configure(config):
     for _ in range(10):
         try:
-            res = requests.get(TRAEFIK_URL)
+            res = requests.get(TRAEFIK_URL + "/static/ok")
             if res.status_code == 200:
                 break
         except requests.ConnectionError:
@@ -36,20 +49,10 @@ def pytest_configure(config):
 
 @pytest.fixture()
 def plugin():
-    middleware_name = "maintenance"
     plugin_name = "maintenance-response"
     cleanup = []
 
     def _configure(enabled=True, content: None | dict[str, str | bytes | None] = None, only_if: str | None = None):
-        with DYNAMIC.open("rb") as f:
-            dynamic = yaml.safe_load(f)
-
-        el = dynamic
-        for key in ("http", "middlewares", middleware_name, "plugin"):
-            if key not in el:
-                el[key] = {}
-            el = el[key]
-
         new: dict[str, Any] = {
             # Traefik does not properly handle booleans in YAML <-> JSON?
             "enabled": "true" if enabled else "false",
@@ -81,29 +84,75 @@ def plugin():
                 else:
                     raise ValueError(f"Content for {name} must be bytes or str, got {type(content_value)}")
 
-        el[plugin_name] = new
+        while True:
+            h = random.randbytes(10).hex()
+            middleware_name = f"maintenance{h}"
+            pattern = f"{REDIS_ROOT_KEY}/http/middlewares/{middleware_name}/*"
+            _, keys = redis.scan(match=pattern, count=1)
+            if keys:
+                continue
+            else:
+                break
 
-        with DYNAMIC.open("w") as f:
-            yaml.safe_dump(dynamic, f, default_flow_style=False, sort_keys=False)
+        def flatten(config) -> list[tuple[str, str]] | str:
+            result = []
+            stack = []
+            if isinstance(config, dict):
+                for key, value in config.items():
+                    stack.append((key, value))
+            elif isinstance(config, list):
+                for i, item in enumerate(config):
+                    stack.append((str(i), item))
+            elif isinstance(config, str | float | int):
+                return str(config)
+            elif isinstance(config, bool):
+                return str(config).lower()
+            else:
+                raise ValueError(f"Unsupported type {type(config)} in config: {config}")
+            for key, value in stack:
+                value = flatten(value)
+                if isinstance(value, str):
+                    result.append((key, value))
+                else:
+                    for sub_key, sub_value in value:
+                        result.append((f"{key}/{sub_key}", sub_value))
+            return result
+
+        if new:
+            tuples = flatten(new)
+            assert isinstance(tuples, list), "Flattening did not produce a list of tuples."
+            flattened = dict(tuples)
+            assert len(flattened) == len(tuples), "Flattening produced duplicate keys."
+        else:
+            flattened = {"": ""}
+
+        route = f"{REDIS_ROOT_KEY}/{ECHO_ROUTER}/middlewares"
+
+        pipe = redis.pipeline()
+        redis_keys = {
+            f"{REDIS_ROOT_KEY}/http/middlewares/{middleware_name}/plugin/{plugin_name}/{k}".removesuffix("/"): v
+            for k, v in flattened.items()
+        }
+        redis_keys[f"{route}/0"] = middleware_name + "@redis"
+        pipe.mset(redis_keys)
+        pipe.execute()
 
         for _ in range(10):
-            middleware = get_middleware_config(f"{middleware_name}@file")
+            middleware = get_middleware_config(f"{middleware_name}@redis")
             if middleware is not None:
                 config = middleware["plugin"][plugin_name]
                 if config == new:
                     break
-                print(config)
             time.sleep(0.1)
         else:
-            raise RuntimeError(f"Middleware {middleware_name}@file not found or not updated after 5 attempts.")
+            raise RuntimeError(f"Middleware {middleware_name} found or not updated after 5 attempts.")
 
-    with DYNAMIC.open("rb") as f:
-        original_dynamic = f.read()
     try:
         yield _configure
     finally:
-        with DYNAMIC.open("wb") as f:
-            f.write(original_dynamic)
+        for dir in cleanup:
+            if dir.exists():
+                shutil.rmtree(dir)
 
 
 def get_middleware_config(name: str) -> dict[str, Any] | None:
@@ -112,7 +161,7 @@ def get_middleware_config(name: str) -> dict[str, Any] | None:
 
 
 class Query:
-    def __init__(self, path="/", method="GET"):
+    def __init__(self, path="/ci/", method="GET"):
         if not path.startswith("/"):
             path = "/" + path
         self.response = requests.request(method, TRAEFIK_URL + path)
